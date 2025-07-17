18ed48c88c303bd1ee358310f48032881642c541
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2025-07-16 22:56:06 -0300

    Add files via upload

---
+++ meu-primeiro-site/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2><img class="catec" src="../assets/logo.png">lá, Mundo!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+h1 {
+  font-family: sans-serif;
+  color: blue;
+  text-align: center;
+}
+h2 {
+  font-family: sans-serif;
+  color: brown;
+  text-align: center;
+  font-size: 200px;
+}
+div.hello {
+  background-color: beige;
+}
+</style>
