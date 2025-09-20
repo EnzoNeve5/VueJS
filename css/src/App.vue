80edee5af8c6592a54812003d3a287926fda2d7e
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2025-09-20 11:10:32 -0300

    Add files via upload

---
+++ css/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="card">
+        Teste
+    </div>
+    <BaseCard />
+    <BaseCard />
+  </div>
+</template>
+
+<script>
+import BaseCard from './components/BaseCard.vue';
+export default {
+  name: 'App',
+  components: { BaseCard },
+  data() {
+    return {
+      
+    }
+  },
+  beforeUpdate() {},
+  updated() {},
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  watch: {},
+  computed: {},
+  methods: {},
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ css/src/components/BaseCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="card">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint in ea voluptates dicta dolor mollitia nostrum eum fugiat excepturi totam veritatis, quis asperiores impedit ab sit est enim aliquam quibusdam?
+    </div>
+</template>
+
+<style scoped>
+.card {
+    background: bisque;
+    padding: 10px;
+    border-radius: 10px;
+}
+</style>
