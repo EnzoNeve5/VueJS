66a32539d2039ece0812d2111665c987fbbe8648
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2025-09-19 21:01:42 -0300

    Add files via upload

---
+++ componentes-slot/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <TheHeader>
+      <template v-slot:description>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque repellat tempore accusamus odio saepe esse rerum possimus, facere corporis, id iusto dolore ad veritatis omnis ipsa explicabo! Totam, debitis mollitia.</p>
+      </template>
+      <p>This is the main content of the header.</p>
+    </TheHeader>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+
+export default {
+  name: 'App',
+  components: { TheHeader },
+  data() {
+    return {
+    };
+  },
+  beforeUpdate() {},
+  updated() {},
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  watch: {},
+  computed: {},
+  methods: {},
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ componentes-slot/src/components/TheHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <header class="header">
+        <h1 
+            v-if="$slots.title"
+            class="title">
+            <slot name="title"></slot>
+        </h1>
+        <div class="description">
+            <slot name="description"></slot>
+        </div>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log(this.$el);
+            console.log(this.$slots.description);
+        }
+    }
+</script>
+
+<style>
+.header {
+    height: 80px;
+    line-height: 80px;
+    text-align: left;
+    color: #0a6aff;
+    background-color: #fffaaa;
+    font-size: 40px;
+    font-family: serif;
+}
+</style>
