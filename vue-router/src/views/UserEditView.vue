58890a5a24c82172a451e3980541de0053a2cbdf
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2025-10-13 13:06:44 -0300

    Add files via upload

---
+++ vue-router/src/views/UserEditView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <pre>
+            {{ $route.query.page }}
+        </pre>
+        Usuário <br>
+
+        <button @click="login()">
+            Login
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log(this.$route);
+        console.log(this.$router);
+    },
+    methods: {
+        login() {
+            // ajax login
+            this.$router.push({ name: 'about' });
+            console.log('chegou');
+        }
+    }
+}
+</script>
